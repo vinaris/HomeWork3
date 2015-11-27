c7f8b1f034ea8aaa9b3776bbf58df544bcbbd925
Валерий Касперович <link206@yandex.ru>
2015-11-27 17:03:04 +0700

    pre

---
+++ MITA.Debaters/Model/Gambler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Gambler
+    {
+        protected Gambler(string name)
+        {
+            Name = name;
+        }
+        public string Name { get; }
+        public double Score { get; private set; }
+    }
+}
